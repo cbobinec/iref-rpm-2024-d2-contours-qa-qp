4e2861cd72fb9190afe7e3a5b8ae79a939dc1c73
ggj62i <cedric.bobinec@insee.fr>
2025-05-26 07:35:10 +0000

    basic generation pmtiles

--- carto/0_init.sh
+++ carto/0_init.sh
@@ -5,3 +5,6 @@ sudo apt install p7zip-full
 # tippecanoe
 sudo apt-get install git build-essential libsqlite3-dev zlib1g-dev --yes
 sudo git clone https://github.com/felt/tippecanoe.git &&  cd tippecanoe && make -j && make install
+
+# transform data
+sudo apt-get install gdal-bin
--- carto/1_prepare_data.sh
+++ carto/1_prepare_data.sh
@@ -3,3 +3,9 @@
 7z e quartiers_pauvres.7z
 
 # transform for tippecanoe
+ogr2ogr quartiers_pauvres.geojson quartiers_pauvres.gpkg
+ogr2ogr quartiers_aises.geojson quartiers_aises.gpkg
+
+# tiles
+tippecanoe quartiers_aises.geojson -o carto/quartiers_aises.pmtiles
+tippecanoe quartiers_pauvres.geojson -o carto/quartiers_pauvres.pmtiles
