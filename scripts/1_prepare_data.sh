400bc8f89715346e719e0ce9efced23a96f118b9
Cédric Bobinec <cedric.bobinec@insee.fr>
2025-05-26 15:47:58 +0200

    ménage/rangement

---
+++ scripts/1_prepare_data.sh
@@ -0,0 +1,11 @@
+# extract 7z archives
+7z e quartiers_aises.7z
+7z e quartiers_pauvres.7z
+
+# transform to geojson for tippecanoe
+ogr2ogr quartiers_pauvres.geojson quartiers_pauvres.gpkg
+ogr2ogr quartiers_aises.geojson quartiers_aises.gpkg
+
+# creation tiles (pmtiles)
+tippecanoe quartiers_aises.geojson -o carto/quartiers_aises.pmtiles
+tippecanoe quartiers_pauvres.geojson -o carto/quartiers_pauvres.pmtiles
