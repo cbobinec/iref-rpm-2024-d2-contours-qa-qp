9ab7ac30b048878457b2dda3011c98035060e33e
Cédric Bobinec <cedric.bobinec@insee.fr>
2025-05-26 15:35:38 +0200

    remise propre

--- carto/0_init.sh
+++ carto/0_init.sh
@@ -2,9 +2,9 @@
 sudo apt update
 sudo apt install p7zip-full
 
-# tippecanoe
+# transform data to geojson
+sudo apt-get install gdal-bin
+
+# tippecanoe : transform data to pmtiles
 sudo apt-get install git build-essential libsqlite3-dev zlib1g-dev --yes
 sudo git clone https://github.com/felt/tippecanoe.git &&  cd tippecanoe && make -j && make install
-
-# transform data
-sudo apt-get install gdal-bin
