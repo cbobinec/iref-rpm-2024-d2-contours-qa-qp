d66b71ee5d873dff92a7b39d72452f7c0b9b75b3
ggj62i <cedric.bobinec@insee.fr>
2025-05-26 07:06:39 +0000

    preparation des données pour carto

---
+++ carto/0_init.sh
@@ -0,0 +1,7 @@
+# 7z utils
+sudo apt update
+sudo apt install p7zip-full
+
+# tippecanoe
+sudo apt-get install git build-essential libsqlite3-dev zlib1g-dev --yes
+sudo git clone https://github.com/felt/tippecanoe.git &&  cd tippecanoe && make -j && make install
---
+++ carto/1_prepare_data.sh
@@ -0,0 +1,5 @@
+# extract
+7z e quartiers_aises.7z
+7z e quartiers_pauvres.7z
+
+# transform for tippecanoe
